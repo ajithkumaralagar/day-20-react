6d4f8f8fc716bbce5553a7903421a9d0c62e01b2
ajithkumaralagar <ajialagar1@gmail.com>
2024-08-01 12:30:00 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  return <> 
+        <div className="container px-5 my-5">
+  <div className="row justify-content-center">
+    <div className="col-lg-8">
+      <div className="card border-0 rounded-3 shadow-lg">
+        <div className="card-body p-4">
+          <div className="text-center">
+            <div className="h1 fw-light">Contact Form</div>
+            <p className="mb-4 text-muted">A functional, Bootstrap 5 contact form with validation built using SB Forms. Try it out!</p>
+          </div>
+
+
+          <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+
+           
+            <div className="form-floating mb-3">
+              <input className="form-control" id="name" type="text" placeholder="Name" data-sb-validations="required" />
+              <label htmlFor="name">Name</label>
+              <div className="invalid-feedback" data-sb-feedback="name:required">Name is required.</div>
+            </div>
+
+           
+            <div className="form-floating mb-3">
+              <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required,email" />
+              <label htmlFor="emailAddress">Email Address</label>
+              <div className="invalid-feedback" data-sb-feedback="emailAddress:required">Email Address is required.</div>
+              <div className="invalid-feedback" data-sb-feedback="emailAddress:email">Email Address Email is not valid.</div>
+            </div>
+
+
+            <div className="form-floating mb-3">
+              <textarea className="form-control" id="message" type="text" placeholder="Message" style={{height:"10rem"}} data-sb-validations="required"></textarea>
+              <label htmlFor="message">Message</label>
+              <div className="invalid-feedback" data-sb-feedback="message:required">Message is required.</div>
+            </div>
+
+            
+            <div className="d-none" id="submitSuccessMessage">
+              <div className="text-center mb-3">
+                <div className="fw-bolder">Form submission successful!</div>
+                <p>To activate this form, sign up at</p>
+                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+              </div>
+            </div>
+
+           
+            <div className="d-none" id="submitErrorMessage">
+              <div className="text-center text-danger mb-3">Error sending message!</div>
+            </div>
+
+           
+            <div className="d-grid">
+              <button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button>
+            </div>
+          </form>
+         
+
+        </div>
+      </div>
     </div>
-  );
+  </div>
+</div>
+
+  </>
+
 }
 
 export default App;
